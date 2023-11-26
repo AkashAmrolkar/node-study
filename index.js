931f3a1574a038af23948d18489a8fbe279e344f
akash <akashamrolkar10@gmail.com>
2023-11-26 13:53:30 +0530

    dotenv

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
+const dotenv = require('dotenv')
 console.log("Hello World...!");
-
+dotenv.config()
 // process.exit() 
 //process.exit() method instructs Node.js to terminate the process synchronously with an exit status of code. If code is omitted, exit uses either the 'success' code 0 or the value of process.exitCode if it has been set. Node.js will not terminate until all the 'exit' event listeners are called.process.exit() method instructs Node.js to terminate the process synchronously with an exit status of code. If code is omitted, exit uses either the 'success' code 0 or the value of process.exitCode if it has been set. Node.js will not terminate until all the 'exit' event listeners are called.process.exit() method instructs Node.js to terminate the process synchronously with an exit status of code. If code is omitted, exit uses either the 'success' code 0 or the value of process.exitCode if it has been set. Node.js will not terminate until all the 'exit' event listeners are called.
-process.exit(1); 
-process.exit(0)
+
+//process.exit(1); 
+//process.exit(0)
+
+
+// process.env() 
+// The process.env property returns an object containing the user environment.
+console.log(process.env.NAME)
+console.log(process.env.Designation)
