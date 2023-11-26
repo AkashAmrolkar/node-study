2fadc2637edc7f5ca7c18947eb29c5e689a0c201
akash <akashamrolkar10@gmail.com>
2023-11-26 13:38:40 +0530

    process.exit

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 console.log("Hello World...!");
+
+// process.exit() 
+//process.exit() method instructs Node.js to terminate the process synchronously with an exit status of code. If code is omitted, exit uses either the 'success' code 0 or the value of process.exitCode if it has been set. Node.js will not terminate until all the 'exit' event listeners are called.process.exit() method instructs Node.js to terminate the process synchronously with an exit status of code. If code is omitted, exit uses either the 'success' code 0 or the value of process.exitCode if it has been set. Node.js will not terminate until all the 'exit' event listeners are called.process.exit() method instructs Node.js to terminate the process synchronously with an exit status of code. If code is omitted, exit uses either the 'success' code 0 or the value of process.exitCode if it has been set. Node.js will not terminate until all the 'exit' event listeners are called.
+process.exit(1); 
+process.exit(0)
